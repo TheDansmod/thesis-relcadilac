00d1e5877176df94dee36ac21da393549a270a7f
Danish Shakeel Mohammad <mohammadsdanish@gmail.com>
2025-11-22 12:32:47 +0000

    done with experiments and graphs for ancestral admgs for sample size and number of nodes variation

--- relcadilac/experiments.py
+++ relcadilac/experiments.py
@@ -15,15 +15,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
-from relcadilac.data_generator import GraphGenerator
-from relcadilac.relcadilac import relcadilac as rel_admg
-from relcadilac.metrics import get_admg_metrics, get_pag_metrics
-from relcadilac.utils import draw_admg, get_ananke_bic
-from gfci.gfci import gfci_search
-from dcd.admg_discovery import Discovery
+# from relcadilac.data_generator import GraphGenerator
+# from relcadilac.relcadilac import relcadilac as rel_admg
+# from relcadilac.metrics import get_admg_metrics, get_pag_metrics
+# from relcadilac.utils import draw_admg, get_ananke_bic
+# from gfci.gfci import gfci_search
+# from dcd.admg_discovery import Discovery
 
 def num_nodes_variation():
-    num_nodes_list = [5, 10, 15, 20, 30, 50]
+    num_nodes_list = [5, 10, 15, 20, 30]
     experiment_data = []
     for n_nodes in num_nodes_list:
         for i in range(5):
@@ -177,8 +177,143 @@ def create_sample_size_plots(file_path='runs/run_004.json'):
     ax.set_title('DCD vs Relcadilac, ADMG SHD and Runtime, Ancestral Graphs')
     plt.savefig('diagrams/dcd_rel_shd_runtime.png')
     plt.close()
+    # dcd Relcadilac, gfci (pag graphs)
+    types = ['skeleton', 'circle', 'head', 'tail']
+    legend_elements_colours = [
+        Line2D([0], [0], color='red', lw=2, label='tpr'),
+        Line2D([0], [0], color='green', lw=2, label='fdr'),
+        Line2D([0], [0], color='blue', lw=2, label='f1'),
+    ]
+    legend_elements_styles = [
+        Line2D([0], [0], color='black', lw=2, linestyle='-', label='Relcadilac'),
+        Line2D([0], [0], color='black', lw=2, linestyle='--', label='DCD'),
+        Line2D([0], [0], color='black', lw=2, linestyle=':', label='GFCI')
+    ]
+    for tp in types:
+        fig, ax = plt.subplots(figsize=(16, 9))
+        ax.plot(sample_sizes, df_new[f'relcadilac_pag_metrics_{tp}_tpr'], '-r')
+        ax.plot(sample_sizes, df_new[f'relcadilac_pag_metrics_{tp}_fdr'], '-g')
+        ax.plot(sample_sizes, df_new[f'relcadilac_pag_metrics_{tp}_f1'], '-b')
+        ax.plot(sample_sizes, df_new[f'dcd_pag_metrics_{tp}_tpr'], '--r')
+        ax.plot(sample_sizes, df_new[f'dcd_pag_metrics_{tp}_fdr'], '--g')
+        ax.plot(sample_sizes, df_new[f'dcd_pag_metrics_{tp}_f1'], '--b')
+        ax.plot(sample_sizes, df_new[f'gfci_pag_metrics_{tp}_tpr'], ':r')
+        ax.plot(sample_sizes, df_new[f'gfci_pag_metrics_{tp}_fdr'], ':g')
+        ax.plot(sample_sizes, df_new[f'gfci_pag_metrics_{tp}_f1'], ':b')
+        ax.add_artist(ax.legend(handles=legend_elements_colours, loc='upper left', title='Metrics'))
+        ax.legend(handles=legend_elements_styles, loc='lower left', title='Algorithm')
+        ax.set_xlabel('Number of Samples')
+        ax.set_ylabel('Value')
+        ax.set_title('DCD vs Relcadilac vs GFCI, PAG Metrics, Ancestral Graphs')
+        plt.savefig(f'diagrams/dcd_rel_gfci_{tp}_pag_metrics.png')
+        plt.close()
+
+def create_num_nodes_plot(file_path='runs/run_003.json'):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    df = get_df_from_runs(data)
+    # df.to_csv('runs/run_003.csv')
+    averaged_rows = []
+    num_nodes_list = [5, 10, 15, 20, 30]
+    for num_nodes in num_nodes_list:
+        averaged_rows.append(df.query(f'num_nodes == {num_nodes}').mean(axis=0, numeric_only=True))
+    df_new = pd.DataFrame(averaged_rows)
+    # df_new.to_csv('runs/run_003_avg.csv')
+    # tpr fdr f1
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(num_nodes_list, df_new['relcadilac_admg_metrics_admg_tpr'], '-r')
+    ax.plot(num_nodes_list, df_new['relcadilac_admg_metrics_admg_fdr'], '-g')
+    ax.plot(num_nodes_list, df_new['relcadilac_admg_metrics_admg_f1'], '-b')
+    ax.plot(num_nodes_list, df_new['dcd_admg_metrics_admg_tpr'], '--r')
+    ax.plot(num_nodes_list, df_new['dcd_admg_metrics_admg_fdr'], '--g')
+    ax.plot(num_nodes_list, df_new['dcd_admg_metrics_admg_f1'], '--b')
+    legend_elements_colours = [
+        Line2D([0], [0], color='red', lw=2, label='tpr'),
+        Line2D([0], [0], color='green', lw=2, label='fdr'),
+        Line2D([0], [0], color='blue', lw=2, label='f1'),
+    ]
+    legend_elements_styles = [
+        Line2D([0], [0], color='black', lw=2, linestyle='-', label='Relcadilac'),
+        Line2D([0], [0], color='black', lw=2, linestyle='--', label='DCD')
+    ]
+    ax.add_artist(ax.legend(handles=legend_elements_colours, loc='upper left', title='Metrics'))
+    ax.legend(handles=legend_elements_styles, loc='lower left', title='Algorithm')
+    ax.set_xlabel('Number of Nodes')
+    ax.set_ylabel('Value')
+    ax.set_title('DCD vs Relcadilac, ADMG Metrics, Ancestral Graphs')
+    plt.savefig('diagrams/dcd_rel_tpr_fdr_f1_admg.png')
+    plt.close()
+    print('dcd_rel_tpr_fdr_f1_admg plotted')
+    # skeleton tpr fdr f1
+    fig, ax = plt.subplots(figsize=(16, 9))
+    ax.plot(num_nodes_list, df_new['relcadilac_admg_metrics_skeleton_tpr'], '-r')
+    ax.plot(num_nodes_list, df_new['relcadilac_admg_metrics_skeleton_fdr'], '-g')
+    ax.plot(num_nodes_list, df_new['relcadilac_admg_metrics_skeleton_f1'], '-b')
+    ax.plot(num_nodes_list, df_new['dcd_admg_metrics_skeleton_tpr'], '--r')
+    ax.plot(num_nodes_list, df_new['dcd_admg_metrics_skeleton_fdr'], '--g')
+    ax.plot(num_nodes_list, df_new['dcd_admg_metrics_skeleton_f1'], '--b')
+    ax.add_artist(ax.legend(handles=legend_elements_colours, loc='upper left', title='Metrics'))
+    ax.legend(handles=legend_elements_styles, loc='lower left', title='Algorithm')
+    ax.set_xlabel('Number of Nodes')
+    ax.set_ylabel('Value')
+    ax.set_title('DCD vs Relcadilac, ADMG Skeleton Metrics, Ancestral Graphs')
+    plt.savefig('diagrams/dcd_rel_tpr_fdr_f1_admg_skeleton.png')
+    plt.close()
+    print('dcd_rel_tpr_fdr_f1_admg_skeleton plotted')
+    # shd runtime
+    fig, ax = plt.subplots(figsize=(16, 9))
+    ax.plot(num_nodes_list, df_new['relcadilac_admg_metrics_admg_shd'], '-b')
+    ax.plot(num_nodes_list, df_new['dcd_admg_metrics_admg_shd'], '--b')
+    ax.set_ylabel('Structural Hamming Distance (SHD)')
+    ax1 = ax.twinx()
+    ax1.plot(num_nodes_list, df_new['relcadilac_time_sec'], '-r')
+    ax1.plot(num_nodes_list, df_new['dcd_time_sec'], '--r')
+    ax1.set_ylabel('Runtime (sec)')
+    legend_elements_colours = [
+        Line2D([0], [0], color='red', lw=2, label='SHD'),
+        Line2D([0], [0], color='blue', lw=2, label='Runtime'),
+    ]
+    ax.add_artist(ax.legend(handles=legend_elements_colours, loc='upper left', title='Metrics'))
+    ax.legend(handles=legend_elements_styles, loc='lower left', title='Algorithm')
+    ax.legend(handles=legend_elements_styles, loc='lower right', title='Algorithm')
+    ax.set_xlabel('Number of Nodes')
+    ax.set_title('DCD vs Relcadilac, ADMG SHD and Runtime, Ancestral Graphs')
+    plt.savefig('diagrams/dcd_rel_shd_runtime.png')
+    plt.close()
+    print('dcd_rel_shd_runtime plotted')
+    # dcd Relcadilac, gfci (pag graphs)
+    types = ['skeleton', 'circle', 'head', 'tail']
+    legend_elements_colours = [
+        Line2D([0], [0], color='red', lw=2, label='tpr'),
+        Line2D([0], [0], color='green', lw=2, label='fdr'),
+        Line2D([0], [0], color='blue', lw=2, label='f1'),
+    ]
+    legend_elements_styles = [
+        Line2D([0], [0], color='black', lw=2, linestyle='-', label='Relcadilac'),
+        Line2D([0], [0], color='black', lw=2, linestyle='--', label='DCD'),
+        Line2D([0], [0], color='black', lw=2, linestyle=':', label='GFCI')
+    ]
+    for tp in types:
+        fig, ax = plt.subplots(figsize=(16, 9))
+        ax.plot(num_nodes_list, df_new[f'relcadilac_pag_metrics_{tp}_tpr'], '-r')
+        ax.plot(num_nodes_list, df_new[f'relcadilac_pag_metrics_{tp}_fdr'], '-g')
+        ax.plot(num_nodes_list, df_new[f'relcadilac_pag_metrics_{tp}_f1'], '-b')
+        ax.plot(num_nodes_list, df_new[f'dcd_pag_metrics_{tp}_tpr'], '--r')
+        ax.plot(num_nodes_list, df_new[f'dcd_pag_metrics_{tp}_fdr'], '--g')
+        ax.plot(num_nodes_list, df_new[f'dcd_pag_metrics_{tp}_f1'], '--b')
+        ax.plot(num_nodes_list, df_new[f'gfci_pag_metrics_{tp}_tpr'], ':r')
+        ax.plot(num_nodes_list, df_new[f'gfci_pag_metrics_{tp}_fdr'], ':g')
+        ax.plot(num_nodes_list, df_new[f'gfci_pag_metrics_{tp}_f1'], ':b')
+        ax.add_artist(ax.legend(handles=legend_elements_colours, loc='upper left', title='Metrics'))
+        ax.legend(handles=legend_elements_styles, loc='lower left', title='Algorithm')
+        ax.set_xlabel('Number of Nodes')
+        ax.set_ylabel('Value')
+        ax.set_title('DCD vs Relcadilac vs GFCI, PAG Metrics, Ancestral Graphs')
+        plt.savefig(f'diagrams/dcd_rel_gfci_{tp}_pag_metrics.png')
+        plt.close()
+        print(f'dcd_rel_gfci_{tp}_pag_metrics plotted')
 
 if __name__ == '__main__':
     seed = 20
-    generator = GraphGenerator(seed)
-    create_sample_size_plots()
+    # generator = GraphGenerator(seed)
+    create_num_nodes_plot()
--- runs/explanations.md
+++ runs/explanations.md
@@ -1,4 +1,4 @@
 1. Run 01 was 1 run for ancestral admgs sample sizes 500, 1000, 2000, 4000 - for relcadilac, dcd, gfci
 2. Run 02 will be for 4 runs for the same as point 1 - so that in total they make 5 runs - can use commit b4a60cfd21c12864070a8a93fb07e6bb39521382 as reference
-3. Run 03 will be for ancestral admgs varying the number of nodes [5, 10, 15, 20, 30, 50] - for relcadilac, dcd, gfci - can use commit 80a3c11c7c8103f8b00cc6c740584b8 as reference
+3. Run 03 will be for ancestral admgs varying the number of nodes [5, 10, 15, 20, 30] - for relcadilac, dcd, gfci - can use commit 80a3c11c7c8103f8b00cc6c740584b8 as reference
 4. Run 04 is not a separate run, I have just combined the data from point 1 and point 2 to make a 5 run set for ancestral admgs with sample sizes 500, 1000, 2000, 4000 for relcadilac, dcd, gfci
